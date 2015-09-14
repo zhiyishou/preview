c1b314f0789e43e1de6181d28005f9e797f453bb
zhiyishou <liangzheng63@163.com>
2015-09-15 07:45:23 +0800

    small optimize

--- draw.py
+++ draw.py
@@ -4,15 +4,24 @@ img = Image.loadImage("./img/mao.jpg")
 img = Image.resizeImage(img,100,100)
 
 data = img.getdata()
+dataList = list(data)
+len = len(dataList)
 colors = []
 
-print(list(data))
+# print(len(list(data)))
 
-for i in range(0, img.size[0]):
-    for j in range(0, img.size[1]):
-        color = data[i * 4 + j]
+# for i in range(0, img.size[0]):
+#     for j in range(0, img.size[1]):
+#         color = data[i * 4 + j]
+#
+#         average = (color[0] + color[1] + color[2]) / 3
+#         colors.append(int(average))
+#
+# print(img.size[0]*img.size[1])
+# print(colors)
 
-        average = (color[0] + color[1] + color[2]) / 3
-        colors.append(int(average))
+for i in range(0,len)
+    color = dataList[i]
 
-print(colors)
+    average = (color[0] + color[1] + color[2]) / 3
+    colors.append(int(average))
